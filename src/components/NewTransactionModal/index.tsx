b53f431cb930d5d1a38f70d6491c2df4cf032c08
Danilonbs <112011887+Danilonbs@users.noreply.github.com>
2023-07-28 11:15:26 -0300

    operation of the buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal'
+import incomeIMG from '../../assets/income.svg'
+import outcomeIMG from '../../assets/outcome.svg'
 import closeIMG from '../../assets/close.svg'
-import { Container } from './styles'
+import { Container, RadioBox, TransactionTypeContainer } from './styles'
+import { useState } from 'react'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -8,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal ({isOpen, onRequestClose } : NewTransactionModalProps) {
+    const [type, setType] = useState('deposit')
+
     return (
         <Modal 
             isOpen={isOpen} 
@@ -33,6 +38,29 @@ export function NewTransactionModal ({isOpen, onRequestClose } : NewTransactionM
                     type='number'
                     placeholder='Valor'
                     />
+
+                    <TransactionTypeContainer>
+                        <RadioBox
+                            type='button'
+                            onClick={() => { setType ('deposit'); }}
+                            isActive={type === 'deposit'}
+                            
+                        >
+                            <img src={incomeIMG} alt="Entrada" />
+                            <span>Entrada</span>
+
+                        </RadioBox>
+                        <RadioBox
+                            type='button'
+                            onClick={() => setType('withdraw')}
+                            isActive={type === 'withdraw'}
+                        >
+                            <img src={outcomeIMG} alt="Saida" />
+                            <span>Saida</span>
+
+                        </RadioBox>
+
+                    </TransactionTypeContainer>
 
                     <input 
                     placeholder='Categoria'
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-
+import { darken } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -51,3 +51,50 @@ export const Container = styled.form`
     }
 
 `;
+
+
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: flex;
+    gap: 0.5rem;
+
+`
+
+interface RadioBoxProps {
+    isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+                            
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+
+    background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    flex: 1;
+
+    transition: border-color 0.2s;
+
+    &:hover {
+            border-color: ${darken(0.1, '#d7d7d7')};
+        }
+    
+
+    img {
+        width: 20px;
+        height: 20px;
+    }
+
+    span {
+        display: inline-block;
+        margin-left: 1rem;
+        font-size: 1rem;
+        color: var(--text-title);
+    }
+`
